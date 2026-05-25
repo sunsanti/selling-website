d76796af466a374cd206d1af2f99452316b32954
Alen <wincute12454@gmail.com>
2026-05-25 19:37:11 +0700

    chore: add .gitignore and update database config

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',     // user bạn đã tạo
-    password: '1234',
+    password: '123456',
     database: 'sellingweb',
     waitForConnections: true,
     connectionLimit: 10,
